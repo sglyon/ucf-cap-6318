6e478ad079a613a3a983b1df182ab6f73ba85a5f
Spencer Lyon <spencerlyon2@gmail.com>
2021-09-11 10:36:44 -0400

    add index for week04

---
+++ notebooks/week04/00_index.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 69fca040-130d-11ec-3e98-95cda05ae86a
+md"""
+# Week 4
+
+> Computational Analysis of Social Complexity
+>
+> Fall 2021, Spencer Lyon
+
+Welcome to week 4!
+
+This week we will finish our introduction to Graph Theory and start talking about Game Theory.
+
+**Reading**: We will cover concepts from chapters 5-6 of the textbook. Sections to focus on include 5.1-5.3, 6.1-6.10
+"""
+
+# ╔═╡ Cell order:
+# ╟─69fca040-130d-11ec-3e98-95cda05ae86a
