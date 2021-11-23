3161d1270e8522df42f9f0d636a9f8ef65901460
Spencer Lyon <spencerlyon2@gmail.com>
2021-11-23 14:09:35 -0500

    add week14 lecture notes

---
+++ notebooks/week14/00_index.jl
@@ -0,0 +1,26 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2abccb0e-8897-47a8-9151-a34633ee1e7e
+md"""
+# Week 14
+
+> Computational Analysis of Social Complexity
+>
+> Fall 2021, Spencer Lyon
+
+Welcome to week 13!
+
+This week we will continue studying blockchains.
+
+We will build on our knowledge of the Ethereum blockchain and its virutal machine (the Ethereum virtual machine or EVM). We will study the field of decentralized finance (defi) and see how the ERC-20 tokens makes the entire Defi ecosystem work like a set of building blocks.
+
+You can find the jupyter notebook for today's lecture <a href="https://github.com/sglyon/ucf-cap-6318/blob/Fall21/notebooks/week14/01-defi.ipynb" target="_blank">here</a>
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─2abccb0e-8897-47a8-9151-a34633ee1e7e
