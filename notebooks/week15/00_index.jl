617ab9025f9c0cfda8814cc84c3b78d71d79a16a
Spencer Lyon <spencerlyon2@gmail.com>
2021-11-30 15:30:39 -0500

    add nft materials

---
+++ notebooks/week15/00_index.jl
@@ -0,0 +1,24 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2abccb0e-8897-47a8-9151-a34633ee1e7e
+md"""
+# Week 15
+
+> Computational Analysis of Social Complexity
+>
+> Fall 2021, Spencer Lyon
+
+Welcome to week 15! This is it -- our last lecture. Today we will finish up our introduction to blockchains by studying the ERC-721 token standard -- also known as NFTs.
+
+NFTs are a very rapidly expanding application of blockchain technology. Be sure to keep an eye on this space in the coming months/years as the world is still figuring out just how the NFT concept can be applied in a wide variety of arenas.
+
+You can find the jupyter notebook for today's lecture <a href="https://github.com/sglyon/ucf-cap-6318/blob/Fall21/notebooks/week15/01-nft.ipynb" target="_blank">here</a>
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─2abccb0e-8897-47a8-9151-a34633ee1e7e
